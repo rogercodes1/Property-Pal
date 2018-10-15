6b50875312bb98d0d5afe9d8b56a416fd2d50351
rogercodes1 <rogerperez.us@gmail.com>
2018-10-14 22:19:54 -0700

    altered location of unsued comments to test if they are needed.

--- config/routes.rb
+++ config/routes.rb
@@ -11,17 +11,18 @@ Rails.application.routes.draw do
   get '/sectors/:id/units', to: 'unit#show'
   get '/about', to: 'static#about'
 
-
-  # get "/tenant_login", to: 'tenants#tenant_login'
   get "/login", to: "sessions#new", as: "login"
   post "/logout", to: 'sessions#destroy'
   get "/logout", to: 'sessions#destroy'
 
   post "/sessions", to: "sessions#create", as: "sessions"
-  # get "/dashboard", to: 'tenants#dashboard'
   get "/admin-dashboard", to: 'admins#dashboard' #, as: "dashboard"
   get "/tenant-dashboard", to: 'tenants#dashboard'
+
+  # get "/tenant_login", to: 'tenants#tenant_login'
+  # get "/dashboard", to: 'tenants#dashboard'
   # get '/tenants/:id/dashboard', to: 'tenants#dashboard', as: 'tenants_dashboard'
+  
   get "pay_rent", to: "tenants#pay_rent"
 
   get "/admins_redir_from_dash", to: "admins#redir_from_dash"
