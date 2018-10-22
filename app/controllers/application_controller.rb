fe55ea442460533fadcb20b3118cd75537536395
rogercodes1 <rogerperez.us@gmail.com>
2018-10-21 19:03:30 -0700

    updated comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_admin
   helper_method :admin_logged_in?
   helper_method :tenant_logged_in?
-  
+
   def current_admin
     if session[:admin_id]
       Admin.find(session[:admin_id])
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def admin_logged_in?
     !!current_admin #double negation, if true user exists, false if it doesn't
-    #!<user instance> --> falase
+    #!<user instance> --> false
     # !nil --> true (negate taht to a false)
   end
   def current_tenant
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   end
   def tenant_logged_in?
     !!current_tenant #double negation, if true user exists, false if it doesn't
-    #!<user instance> --> falase
+    #!<user instance> --> false
     # !nil --> true (negate taht to a false)
   end
 
